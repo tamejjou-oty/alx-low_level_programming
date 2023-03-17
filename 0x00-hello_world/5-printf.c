5237c7eb56dffd556d734937386e5f9c296c43fc
tamejjou othmane <tamejjouothmane@gmail.com>
2023-03-16 18:33:45 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
-#include <studio.h>
+#include <stdio.h>
+
 /**
  * main - Prints a sentence
  * Return: returns 0.
  */
-int main(void) {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return 0;
+ int main(void) {
+ 
+	 printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }

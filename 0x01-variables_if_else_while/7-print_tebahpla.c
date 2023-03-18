22a78cbaa910f0b36cf132f382f9e9c9f5a8245b
tamejjou othmane <tamejjouothmane@gmail.com>
2023-03-17 18:17:09 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints numbers in reverse.
+ * Return: returns 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

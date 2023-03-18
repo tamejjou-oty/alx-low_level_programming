c41b4dab63e28aa0077f7f5d13001c5d1c4c0ffa
tamejjou othmane <tamejjouothmane@gmail.com>
2023-03-17 18:15:43 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints numbers.
+ * Return: returns 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

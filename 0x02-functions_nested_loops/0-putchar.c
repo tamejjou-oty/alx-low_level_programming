4360cecd897093eaab136f70595420706f89492a
tamejjou othmane <tamejjouothmane@gmail.com>
2023-03-21 16:28:02 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,14 +2,11 @@
 #include "main.h"
 #include <string.h>
 int main(void)
-{
-	char string[20] = "_putchar";
-	unsigned int i;
-
-	for (i = 0; i < strlen(string); i++)
-	{
-		_putchar(string[i]);
-	}
-	_putchar('\n');
-	return (0);
+{char string[20] = "_putchar";
+unsigned int i;
+for (i = 0; i < strlen(string); i++)
+{	_putchar(string[i]);
+}
+_putchar('\n');
+return (0);
 }

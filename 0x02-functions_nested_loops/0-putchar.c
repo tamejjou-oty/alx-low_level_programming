f38253f8c08cda3d9b6c3e0f6229d398730562f7
tamejjou othmane <tamejjouothmane@gmail.com>
2023-03-21 16:04:43 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
-
-int main() {
-	printf("_putchar\n");
-	return 0;
-}
+#include "main.h"
+#include <unistd.h>
+	int _putchar(char c)
+	{
+		return (write(1, &c, 1));
+		_putchar('\n');
+	     return (0);
+	}

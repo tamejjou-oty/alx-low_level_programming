624bf989309862d1d613317016da5e1b1bea80ca
tamejjou othmane <tamejjouothmane@gmail.com>
2023-03-21 17:51:55 -0700

    Write a function that prints the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+void print_alphabet(void){
+	char harf = 'a';
+	while (harf <= 'z')
+	{_putchar(harf);
+		harf++;
+	}
+	_putchar('\n');
+	 return (0);
+}

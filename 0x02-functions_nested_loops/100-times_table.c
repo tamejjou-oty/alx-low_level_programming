cdcd57f573c0f323a04f288942b80eca630982f7
tamejjou othmane <tamejjouothmane@gmail.com>
2023-03-21 19:28:08 -0700

    Write a function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,47 @@
 #include "main.h"
+
 /**
- * print_times_table - prints times table starting with 0
- * @n: integer base for table
- *
- * Return: void
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
  */
 void print_times_table(int n)
 {
-	int i, j, prod;
+	int i, j, k;
 
-	if (n <= 15 && n >= 0)
+	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			_putchar('0');
-			for (j = 1; j <= n; j++)
+			for (j = 0; j <= n; j++)
 			{
-				prod = i * j;
-				_putchar(',');
-				_putchar(' ');
-				if (prod <= 99)
+				k = j * i;
+				if (j == 0)
 				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
 					_putchar(' ');
-				}
-				if (prod <= 9)
-				{
 					_putchar(' ');
-					_putchar(prod + '0');
-				}
-				if (prod >= 100)
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
 				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
-					_putchar((prod % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
-				else
-					if (prod >= 10 && prod <= 99)
-					{
-						_putchar((prod / 10) + '0');
-						_putchar((prod % 10) + '0');
-					}
 			}
-		_putchar('\n');
+			_putchar('\n');
 		}
 	}
 }

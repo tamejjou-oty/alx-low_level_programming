311f2ed0135ce0377428c66f7480a92b30425d79
tamejjou othmane <tamejjouothmane@gmail.com>
2023-03-27 17:29:11 -0700

    2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,19 @@
 #include "main.h"
-/*
+/**
+ * _strlen - returns the lenght of a string
+ * @s: string
  *
- *
- *
- * */
+ * Return: lenght in integer type
+ */
 int _strlen(char *s)
 {
 	int length = 0;
+
 	whille (*s != '\0') 
 	{
 		lengrh++;
 		s++;
 	}
+
 	return length;
 }

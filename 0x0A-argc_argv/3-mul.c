9410bc86495be83fe3d85716b87b8571b242b380
tamejjou othmane <tamejjouothmane@gmail.com>
2023-04-10 17:25:54 -0700

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,14 +3,15 @@
 #include "main.h"
 
 /**
- * main - prints all arguments it receives.
+ * main -  multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0
+ * Return: 0 if success, 1 if more than two arguments
  */
+
 int main(int argc, char *argv[])
 {
-	if (argc !=3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return 1;

d4dd1ae175be24ac2d515b18ceeb7838d6d26489
tamejjou othmane <tamejjouothmane@gmail.com>
2023-04-10 17:29:04 -0700

    0x0A. C - argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,8 +17,7 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
+	int num1 = atoi(argv[1]) , num2 = atoi(argv[2]);
 	int redult = nim1 * num2;
 
 	printf("%d\n",result);
